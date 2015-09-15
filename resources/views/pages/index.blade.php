7aa23731adf10e8f246a056e43907d425a72d250
Olof Olsson <borsna@gmail.com>
2015-09-15 11:17:54 +0200

    using " istead of '

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -17,21 +17,21 @@
         <div class="small-12 columns">
             <ul class="featured-grid small-block-grid-1 medium-block-grid-3">
                 <li>
-                    <img src="{{ asset('/images/content/making_nordic_health_data_visible.png') }}" alt="Making Nordic Health Data Visible" />
+                    <img src="{{ asset("/images/content/making_nordic_health_data_visible.png") }}" alt="Making Nordic Health Data Visible" />
 
                     <h2>Making Nordic Health Data Visible</h2>
 
                     <p>The Nordic countries have a solid tradition for gathering high quality survey and administrative data that enable researchers to analyse and understand processes like ageing, socio-economic health differences, health risks and changing welfare service systems. However, locating and accessing the data can be a challenge. We will develop common practices for easier access to data resources across national borders and enhance the visibility of Nordic research in the European context.</p>
                 </li>
                 <li>
-                    <img src="{{ asset('/images/content/portal_for_nordic_health_data.png') }}" alt="Portal for Nordic Health Data" />
+                    <img src="{{ asset("/images/content/portal_for_nordic_health_data.png") }}" alt="Portal for Nordic Health Data" />
 
                     <h2>Portal for Nordic Health Data</h2>
 
                     <p>The main outcome of this project is a web portal prototype that helps researchers search and access the existing Nordic health research data. The portal will build upon the participating data archives’ existing DDI documentation. In order to provide an enhanced user experience, we will harmonize our documentation practices and, for example, expand our common controlled vocabularies to include concepts relevant for health data.</p>
                 </li>
                 <li>
-                    <img src="{{ asset('/images/content/collaboration_with_researchers.png') }}" alt="Collaboration with Researchers" />
+                    <img src="{{ asset("/images/content/collaboration_with_researchers.png") }}" alt="Collaboration with Researchers" />
 
                     <h2>Collaboration with Researchers</h2>
 
