34cb6e03d9d93d948c7f3459af6bc9fca0ffa3b4
Olof Olsson <borsna@gmail.com>
2015-11-04 17:07:45 +0100

    check if keyword and subject is array before looping

--- resources/views/study/view.blade.php
+++ resources/views/study/view.blade.php
@@ -29,7 +29,7 @@
 
         <ul>
             @foreach ($study["_source"]["subject"] as $subject)
-            @if (array_key_exists("en", $subject))
+            @if (is_array($subject) && array_key_exists("en", $subject))
             <li><a href="{{ action('SearchController@search', ['subject' => $subject["en"]]) }}">{{ $subject["en"] }}</a></li>
             @endif
             @endforeach
@@ -40,7 +40,7 @@
         <h3>Keywords</h3>
         <ul>
             @foreach ($study["_source"]["keyword"] as $keyword)
-            @if (array_key_exists("en", $keyword))
+            @if (is_array($keyword) && array_key_exists("en", $keyword))
             <li><a href="{{ action('SearchController@search', ['keyword' => $keyword["en"]]) }}">{{ $keyword["en"] }}</a></li>
             @endif
             @endforeach
