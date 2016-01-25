96557f75c50531d77ec6be42c70ac4a2a0e6b97b
OscarJohansson123 <oscar.johansson.ee14e@gmail.com>
2016-01-25 16:51:04 +0100

    Sidebar

--- resources/views/study/view.blade.php
+++ resources/views/study/view.blade.php
@@ -4,18 +4,18 @@
 <div class="row">
     <div class="small-12 columns">
 
-        <h1>{{ Utils::getEn($study["_source"]["title"]) }}</h1>
+        <h1 class="title" >{{ Utils::getEn($study["_source"]["title"]) }}</h1>
 
         <p><strong>ID:</strong> <span>{{ $study["_id"] }}</span><br /></p>
 
         @if ($study["_source"]["landingpage"])
-        <p><a href="{{ $study["_source"]["landingpage"] }}">{{ $study["_source"]["landingpage"] }}</a></p>
+        <p><a class="linkbutton" href="{{ $study["_source"]["landingpage"] }}">Go to Study</a></p>
         @endif
 
         @if (array_key_exists("abstract", $study["_source"]))
-        <h3>Abstract</h3>
+        <h3 class="Abstract" >Abstract</h3>
 
-        <p>{{ Utils::getEn($study["_source"]["abstract"]) }}</p>
+        <p class="abstract" >{{ Utils::getEn($study["_source"]["abstract"]) }}</p>
         @endif
 
         @if (array_key_exists("purpose", $study["_source"]))
@@ -23,30 +23,30 @@
 
         <p>{{ Utils::getEn($study["_source"]["purpose"]) }}</p>
         @endif
+		<div class="sidebar">
+			@if (array_key_exists("subject", $study["_source"]))
+			<h3>Subjects</h3>
 
-        @if (array_key_exists("subject", $study["_source"]))
-        <h3>Subjects</h3>
+			<ul>
+				@foreach ($study["_source"]["subject"] as $subject)
+				@if (is_array($subject) && array_key_exists("en", $subject))
+				<li><a href="{{ action('SearchController@search', ['subject' => $subject["en"]]) }}">{{ $subject["en"] }}</a></li>
+				@endif
+				@endforeach
+			</ul>
+			@endif
 
-        <ul>
-            @foreach ($study["_source"]["subject"] as $subject)
-            @if (is_array($subject) && array_key_exists("en", $subject))
-            <li><a href="{{ action('SearchController@search', ['subject' => $subject["en"]]) }}">{{ $subject["en"] }}</a></li>
-            @endif
-            @endforeach
-        </ul>
-        @endif
-
-        @if (array_key_exists("keyword", $study["_source"]))
-        <h3>Keywords</h3>
-        <ul>
-            @foreach ($study["_source"]["keyword"] as $keyword)
-            @if (is_array($keyword) && array_key_exists("en", $keyword))
-            <li><a href="{{ action('SearchController@search', ['keyword' => $keyword["en"]]) }}">{{ $keyword["en"] }}</a></li>
-            @endif
-            @endforeach
-        </ul>
-        @endif
-
+			@if (array_key_exists("keyword", $study["_source"]))
+			<h3>Keywords</h3>
+			<ul>
+				@foreach ($study["_source"]["keyword"] as $keyword)
+				@if (is_array($keyword) && array_key_exists("en", $keyword))
+				<li><a href="{{ action('SearchController@search', ['keyword' => $keyword["en"]]) }}">{{ $keyword["en"] }}</a></li>
+				@endif
+				@endforeach
+			</ul>
+			@endif
+		</div>
         <p>
             @if (array_key_exists("startdate", $study["_source"]))
             <strong>Start date:</strong> <span>{{ $study["_source"]["startdate"] }}</span><br />
