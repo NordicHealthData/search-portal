446549a23cc6bd3b2bdb59e86249b19bf5a82347
OscarJohansson123 <oscar.johansson.ee14e@gmail.com>
2016-02-26 16:14:45 +0100

    Better list of search studies
    Search studies are placed under each other. Search filter is on the same
    row now.

--- resources/views/search/simpleSearch.blade.php
+++ resources/views/search/simpleSearch.blade.php
@@ -3,38 +3,43 @@
 @section("content")
     <div class="row">
         <div class="small-12 columns">
-            <h1>Search Health Data</h1>
+            <div class="row">
+                <div class="small-6 columns">
+                    <div class="searchbar">
+                    <h1>Search Health Data</h1>
+                        {!! Form::open(array("action" => "SearchController@search", "method" => "GET")) !!}
+                        {!! Form::text("q", Request::input("q"), array("class" => "search", "autocomplete" => "off", "placeholder" => "Search for Studies...", "data-suggesturl"=> action("SearchController@suggest"))) !!}
 
-            {!! Form::open(array("action" => "SearchController@search", "method" => "GET")) !!}
-            {!! Form::text("q", Request::input("q"), array("class" => "search", "autocomplete" => "off", "placeholder" => "Search for Studies...", "data-suggesturl"=> action("SearchController@suggest"))) !!}
+                        @if (isset($hits->aggregations))
+                            @foreach ($hits->aggregations as $key => $aggregation)
+                                @if (Input::get($key))
+                                    {!! Form::hidden($key, Input::get($key)) !!}
+                                @endif
+                            @endforeach
+                        @endif
+                    </div>
+                </div>
+                <div class="small-6 columns">
+                    {!! Form::submit("Search", array("class" => "button")) !!}
+                    {!! Form::close() !!}
 
-            @if (isset($hits->aggregations))
-                @foreach ($hits->aggregations as $key => $aggregation)
-                    @if (Input::get($key))
-                        {!! Form::hidden($key, Input::get($key)) !!}
-                    @endif
-                @endforeach
-            @endif
-
-            {!! Form::submit("Search", array("class" => "button")) !!}
-            {!! Form::close() !!}
-
-            @foreach ($aggregations as $key => $aggregation)
-                @if (Input::has($key))
-                    @foreach (Utils::getArgumentValues($key) as $value)
-                        @if (Utils::keyValueActive($key, $value))
-                            <a href="{{ route("search", Utils::removeKeyValue($key, $value)) }}" class="label label-info">
-                                <span class="text-info">{{ ucfirst($key) }}:</span> {{ $value }}
-                                <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
-                            </a>
+                    @foreach ($aggregations as $key => $aggregation)
+                        @if (Input::has($key))
+                            @foreach (Utils::getArgumentValues($key) as $value)
+                                @if (Utils::keyValueActive($key, $value))
+                                    <a href="{{ route("search", Utils::removeKeyValue($key, $value)) }}" class="label label-info">
+                                        <span class="text-info">{{ ucfirst($key) }}:</span> {{ $value }}
+                                        <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+                                    </a>
+                                @endif
+                            @endforeach
                         @endif
                     @endforeach
-                @endif
-            @endforeach
-
+                </div>    
+            </div> 
             <h2>Filter Search Results by</h2>
 
-            <ul class="small-block-grid-1 medium-block-grid-7">
+            <ul class="small-block-grid-1 medium-block-grid-8">
                 @foreach ($aggregations as $key => $aggregation)
                     @if (count($hits->aggregations[$key]["buckets"]) > 0 || Input::has($key))
                         <li class = "filter">
@@ -71,9 +76,9 @@
                 @endforeach
             </ul>
 
-            <p><strong>Total:</strong> <span class="badge">{{ $hits->total() }}</span></p>
+            <p class="total"><strong>Total:</strong> <span class="badge">{{ $hits->total() }}</span></p>
 
-            <ul class="small-block-grid-1 medium-block-grid-4">
+            <ul class="small-block-grid-1 medium-block-grid-1">
                 @foreach($hits as $hit)
                     @include("study._study")
                 @endforeach
--- resources/views/study/_study.blade.php
+++ resources/views/study/_study.blade.php
@@ -1,23 +1,24 @@
-<li class="card">
-    <header class="header">
-        <a href="{{ action('StudyController@view', ['id' => $hit["_id"]]) }}">
-            <strong class="label">{{ $hit["_id"] }}</strong>
-            {{ Utils::getEn($hit["_source"]["title"]) }}
-        </a>
-    </header>
 
-    <div class="content">
-        @if(array_key_exists("highlight", $hit))
-            @foreach($hit["highlight"] as $label => $highlight)
-                <p>
-                @foreach(array_keys($highlight) as $key)
-                    <strong>{{ $label }}</strong>: {!!$highlight[$key]!!}
+    <li class="card">
+        <header class="header">
+            <a href="{{ action('StudyController@view', ['id' => $hit["_id"]]) }}">
+                <strong class="label">{{ $hit["_id"] }}</strong>
+                {{ Utils::getEn($hit["_source"]["title"]) }}
+            </a>
+        </header>
+
+        <div class="content">
+            @if(array_key_exists("highlight", $hit))
+                @foreach($hit["highlight"] as $label => $highlight)
+                    <p>
+                    @foreach(array_keys($highlight) as $key)
+                        <strong>{{ $label }}</strong>: {!!$highlight[$key]!!}
+                    @endforeach
+                    </p>
                 @endforeach
-                </p>
-            @endforeach
-        @elseif (array_key_exists("abstract", $hit["_source"]))
-            {{ str_limit(Utils::getEn($hit["_source"]["abstract"]), 150) }}
-        @endif
-    </div>
-    <!-- /content -->
-</li>
+            @elseif (array_key_exists("abstract", $hit["_source"]))
+                {{ str_limit(Utils::getEn($hit["_source"]["abstract"]), 150) }}
+            @endif
+        </div>
+        <!-- /content -->
+    </li>
