20f26a3dace486ba90b32a7647d467788538c59b
jfihn <johanfihn@gmail.com>
2016-03-03 11:22:44 +0100

    Styling of sidebar items

--- resources/views/study/view.blade.php
+++ resources/views/study/view.blade.php
@@ -9,7 +9,7 @@
             @if ($study["_source"]["landingpage"])
             <p>
                 <a class="landingpage" href="{{ $study["_source"]["landingpage"] }}" title="If you have any questions regarding this data set please refer your questions to the archive responsible for the study.">
-                    <i class="fi-link"></i>
+                    <i class="fi-web"></i>
                     Access resource at host archive
                 </a>
             </p>
@@ -84,6 +84,20 @@
     </div>
     <div  class="small-4 columns">
 
+        @if (count($study['related'] < 0))
+        <div>
+            <strong>Related studies</strong>
+            <p>
+                @foreach ($study["related"] as $related)
+                <span class="sidebar-item">
+                    <i class="fi-page"></i><a href="{{ action('StudyController@view', ['id' => $related["_id"]]) }}">{{ Utils::getEn($related["_source"]["title"]) }}</a>
+                </span>
+                <br/>
+                @endforeach
+            </p>
+        </div>
+        @endif
+        
         @if (array_key_exists("subject", $study["_source"]))
         <div>
             <strong>Subjects</strong>
@@ -91,7 +105,9 @@
             <p>
                 @foreach ($study["_source"]["subject"] as $subject)
                 @if (is_array($subject) && array_key_exists("en", $subject))
-                &bullet; <a href="{{ action('SearchController@search', ['subject' => $subject["en"]]) }}">{{ $subject["en"] }}</a>
+                <span class="sidebar-item">
+                    <i class="fi-star"></i> <a href="{{ action('SearchController@search', ['subject' => $subject["en"]]) }}">{{ $subject["en"] }}</a>
+                </span>
                 @endif
                 @endforeach
             </p>
@@ -104,21 +120,12 @@
             <p>
                 @foreach ($study["_source"]["keyword"] as $keyword)
                 @if (is_array($keyword) && array_key_exists("en", $keyword))
-                &bullet; <a href="{{ action('SearchController@search', ['keyword' => $keyword["en"]]) }}">{{ $keyword["en"] }}</a>
+                <span class="sidebar-item">
+                    <i class="fi-burst"></i><a href="{{ action('SearchController@search', ['keyword' => $keyword["en"]]) }}">{{ $keyword["en"] }}</a>
+                </span>
                 @endif
                 @endforeach
             </p>
-        </div>
-        @endif
-
-        @if (count($study['related'] < 0))
-        <div>
-            <strong>Related studies</strong>
-            <ul>
-                @foreach ($study["related"] as $related)
-                <li><a href="{{ action('StudyController@view', ['id' => $related["_id"]]) }}">{{ Utils::getEn($related["_source"]["title"]) }}</a></li>
-                @endforeach
-            </ul>
         </div>
         @endif
     </div>
