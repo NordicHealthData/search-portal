ae6c4313607c65476057d1c9f8dd77b4f7b5fee3
Snorre Magnus Davøen <snorremd@gmail.com>
2016-03-03 09:43:02 +0100

    Fix left column search page styling
    Fixed styling and markup for left search page column so things align.
    Now all things from page header down aligns correctly and consistently
    on the left side.

--- resources/views/search/simpleSearch.blade.php
+++ resources/views/search/simpleSearch.blade.php
@@ -4,9 +4,9 @@
     <div class="row">
         <div class="small-4 columns">
             <div class="row">
-                <div class="searchbar">
+                <div class="searchbar small-12 columns">
                     <h1>Search Health Data</h1>
-                    <div class="small-8 columns">
+                    <div class="small-10 columns">
                         {!! Form::open(array("action" => "SearchController@search", "method" => "GET")) !!}
                         {!! Form::text("q", Request::input("q"), array("class" => "search", "autocomplete" => "off", "placeholder" => "Search for Studies...", "data-suggesturl"=> action("SearchController@suggest"))) !!}
 
@@ -18,7 +18,7 @@
                             @endforeach
                         @endif
                     </div>
-                    <div class="small-4 columns">
+                    <div class="small-2 columns">
                         <button type="submit" class="button">
                                 <i class="fi-magnifying-glass"></i>
                         </button>
@@ -26,63 +26,68 @@
                     </div>
                 </div>
             </div>
-            <ul class="aggregations">
-            @foreach ($aggregations as $key => $aggregation)
-                @if (Input::has($key))
-                    @foreach (Utils::getArgumentValues($key) as $value)
-                        @if (Utils::keyValueActive($key, $value))
-                            <li class="aggregation">
-                                <a href="{{ route("search", Utils::removeKeyValue($key, $value)) }}" class="label label-info">
-                                    <span class="text-info">{{ ucfirst($key) }}:</span> {{ $value }}
-                                    <span class="badge">
-                                        <i class="fi-x"></i>
-                                    </span>
-                                </a>
-                            </li>
-                        @endif
-                    @endforeach
-                @endif
-            @endforeach
-            </ul>
-
-
-                <p><strong>Filter search result by</strong></p>
-
-                <ul>
-                    @foreach ($aggregations as $key => $aggregation)
-                        @if (count($hits->aggregations[$key]["buckets"]) > 0 || Input::has($key))
-                            <strong>{{ $aggregations_title[$key] }}</strong>
-
-                            <ul class="filters">
-                                @if (Input::has($key))
-                                    @foreach (Utils::getArgumentValues($key) as $value)
-                                        @if (Utils::keyValueActive($key, $value))
-                                            <li>
-                                                <a href="{{ route("search", Utils::removeKeyValue($key, $value)) }}" class="active">
-                                                    <span class="label">X</span>
-                                                    {{ $value }}
-                                                </a>
-                                            </li>
-                                        @endif
-                                    @endforeach
-                                @endif
-
-                                @foreach ($hits->aggregations[$key]["buckets"] as $bucket)
-                                    @if (Utils::keyValueActive($key, $bucket["key"]) == false)
-                                        <li>
-                                            <a href="{{ route("search", Utils::addKeyValue($key, $bucket["key"])) }}">
-                                                <span class="label">{{ $bucket["doc_count"] }}</span>
-                                                {{ $bucket["key"] }}
+            <div class="row">
+                    <div class="small-12 columns">
+                                            <ul class="aggregations">
+                        @foreach ($aggregations as $key => $aggregation)
+                            @if (Input::has($key))
+                                @foreach (Utils::getArgumentValues($key) as $value)
+                                    @if (Utils::keyValueActive($key, $value))
+                                        <li class="aggregation">
+                                            <a href="{{ route("search", Utils::removeKeyValue($key, $value)) }}" class="label label-info">
+                                                <span class="text-info">{{ ucfirst($key) }}:</span> {{ $value }}
+                                                <span class="badge">
+                                                    <i class="fi-x"></i>
+                                                </span>
                                             </a>
                                         </li>
                                     @endif
                                 @endforeach
-                            </ul>
-                            <br>
-                            <!-- /filters -->
-                        @endif
-                    @endforeach
-                </ul>
+                            @endif
+                        @endforeach
+                       </ul>
+                    </div>
+                </div>
+            
+
+
+            <p><strong>Filter search result by</strong></p>
+
+            <ul class="filters">
+                @foreach ($aggregations as $key => $aggregation)
+                    @if (count($hits->aggregations[$key]["buckets"]) > 0 || Input::has($key))
+                        <strong>{{ $aggregations_title[$key] }}</strong>
+
+                        <ul class="filters">
+                            @if (Input::has($key))
+                                @foreach (Utils::getArgumentValues($key) as $value)
+                                    @if (Utils::keyValueActive($key, $value))
+                                        <li>
+                                            <a href="{{ route("search", Utils::removeKeyValue($key, $value)) }}" class="active">
+                                                <span class="label">X</span>
+                                                {{ $value }}
+                                            </a>
+                                        </li>
+                                    @endif
+                                @endforeach
+                            @endif
+
+                            @foreach ($hits->aggregations[$key]["buckets"] as $bucket)
+                                @if (Utils::keyValueActive($key, $bucket["key"]) == false)
+                                    <li>
+                                        <a href="{{ route("search", Utils::addKeyValue($key, $bucket["key"])) }}">
+                                            <span class="label">{{ $bucket["doc_count"] }}</span>
+                                            {{ $bucket["key"] }}
+                                        </a>
+                                    </li>
+                                @endif
+                            @endforeach
+                        </ul>
+                        <br>
+                        <!-- /filters -->
+                    @endif
+                @endforeach
+            </ul>
 
 
         </div>
