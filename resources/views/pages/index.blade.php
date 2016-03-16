48d4510d3f90a3287fc87d53b60c04e7e8a2a901
Nils Norman Haukås <nils@thunki.com>
2016-03-16 20:37:09 +0100

    Revert "Updated css. Improved how the menu collapses for mobile view by making the font-size a little smaller. Also changed the headline on the frontpage a little bit to make it not stay flush against the left side."
    This reverts commit 5f8872401dae16df8809cb782e6f263b26061e0f.

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,12 +3,18 @@
 @section("content")
     <div class="featured">
         <div class="row">
+            <!--<div class="small-4 columns">
+                <ul id="frontpage-left-menu"  class="vertical menu">
+                    <li id = "home">{!! link_to("/", "Home", array("title" => "Home")) !!}</li>
+                    <li id = "about">{!! link_to_route("pages.show", "About", array("path" => "about"), array("title" => "About")) !!}</li>
+                    <li id = "search">{!! link_to("/search", "Search Studies", array("title" => "Search")) !!}</li>
+                    
+                </ul>
+            </div>-->
             <div class="small-12 columns">
                 <div class="row">
-                    <div class="small-12 columns">
-                        <h1>Nordic Health<br />
-                        Data Portal <span class="tag prototype">[prototype]</span> <small>&mdash; Making Nordic Health Data Visible</small></h1>
-                    </div>
+                    <h1>Nordic Health<br />
+                    Data Portal <span class="tag prototype">[prototype]</span> <small>&mdash; Making Nordic Health Data Visible</small></h1>
                 </div>
                 <div class="row">
                     <div class="small-12 columns">
