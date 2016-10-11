09ba83de9849a69158a30d08a427820199f34d33
Olof Olsson <borsna@users.noreply.github.com>
2016-10-11 13:22:33 +0200

    fix present

--- resources/views/pages/about/index.blade.php
+++ resources/views/pages/about/index.blade.php
@@ -13,16 +13,16 @@
 
     <div class="row">
         <div class="small-12 medium-7 columns">
-            <p>This project is a collaboration between the national data archives of the Nordic countries: <a href="http://www.nsd.uib.no/nsd/english/index.html" title="Norwegian Social Science Data Services (NSD)">NSD</a>, <a href="http://snd.gu.se/en" title="Swedish National Data Service (SND)">SND</a>, <a href="http://www.fsd.uta.fi/en/" title="Finnish Social Science Data Archive (FSD)">FSD</a> and <a href="http://samfund.dda.dk/dda/default-en.asp" title="Danish Data Archive (DDA)">DDA</a>. <a href="http://www.nordforsk.org/en/news/nordforsk-invests-in-biobank-and-registry-research" title="NordForsk">NordForsk awarded the project a grant</a> of NOK 490 000 for the period 2014‒2016.</p>
+            <p>This project was a collaboration between the national data archives of the Nordic countries: <a href="http://www.nsd.uib.no/nsd/english/index.html" title="Norwegian Social Science Data Services (NSD)">NSD</a>, <a href="http://snd.gu.se/en" title="Swedish National Data Service (SND)">SND</a>, <a href="http://www.fsd.uta.fi/en/" title="Finnish Social Science Data Archive (FSD)">FSD</a> and <a href="http://samfund.dda.dk/dda/default-en.asp" title="Danish Data Archive (DDA)">DDA</a>. <a href="http://www.nordforsk.org/en/news/nordforsk-invests-in-biobank-and-registry-research" title="NordForsk">NordForsk awarded the project a grant</a> of NOK 490 000 for the period 2014‒2016.</p>
 
-            <p>Our objective is to develop common practices for storing and disseminating health-related data across national borders, taking advantage of synergies between the Nordic countries. The project will build a portal prototype for Nordic health data for research purpose and enhance the visibility of Nordic research in a European context. The work will also lay the ground for further collaboration in finding solutions to common challenges and possibilities with regard to data curation, preservation and data sharing in the Nordic countries.</p>
+            <p>Our objective was to develop common practices for storing and disseminating health-related data across national borders, taking advantage of synergies between the Nordic countries. The project will build a portal prototype for Nordic health data for research purpose and enhance the visibility of Nordic research in a European context. The work was also laying the ground for further collaboration in finding solutions to common challenges and possibilities with regard to data curation, preservation and data sharing in the Nordic countries.</p>
 
             <p>For more information, please contact the project leader <a href="http://www.uib.no/personer/Dag.Kiberg" title="University of Bergen: Dag Kiberg">Dag Kiberg</a> of the <a href="http://www.nsd.uib.no/nsd/english/index.html" title="Norwegian Social Science Data Services (NSD)">Norwegian Social Science Data Services (NSD)</a>.</p>
         </div>
         <!-- /small-12.medium-7.columns -->
 
         <aside class="small-12 medium-5 columns">
-            <h3>Upcoming meetings</h3>
+            <h3>Meetings</h3>
 
             <ul class="timeline">
                 <li class="next"><span>Next up</span>Autumn 2016 in Bergen: Closing meeting</li>
