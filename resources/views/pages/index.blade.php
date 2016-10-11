d221d7f3cb0d3c64918f964f187738c1ad3330ae
Olof Olsson <borsna@users.noreply.github.com>
2016-10-11 13:20:33 +0200

    fix for present

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -30,21 +30,21 @@
 
                     <h2>Making Nordic Health Data Visible</h2>
 
-                    <p>This is a project funded by NordForsk. The project period is from autumn 2014 until autumn 2016 and the project is a collaboration between the data archives in Norway, Sweden, Finland and Denmark. The goal is to make a search portal prototype for Nordic health data. For this purpose the portal is covering metadata from the data collections in the four data archives, and builds upon existing DDI documentation.</p>
+                    <p>This was a project funded by NordForsk. The project period was from autumn 2014 until autumn 2016 and the project was a collaboration between the data archives in Norway, Sweden, Finland and Denmark. The goal was to make a search portal prototype for Nordic health data. For this purpose the portal is covering metadata from the data collections in the four data archives, and builds upon existing DDI documentation.</p>
                 </li>
                 <li>
                     <img src="{{ asset("/images/content/portal_for_nordic_health_data.png") }}" alt="Search Portal Prototype" />
 
                     <h2>Search Portal Prototype</h2>
 
-                    <p>The purpose of the prototype is to demonstrate mechanisms to search and retrieve metadata for the catalogued datasets. It allows searching in metadata across repositories and studies, presenting the results in a standardised way.</p>
+                    <p>The purpose of the prototype was to demonstrate mechanisms to search and retrieve metadata for the catalogued datasets. It allows searching in metadata across repositories and studies, presenting the results in a standardised way.</p>
                 </li>
                 <li>
                     <img src="{{ asset("/images/content/collaboration_with_researchers.png") }}" alt="Input" />
 
                     <h2>Input</h2>
 
-                    <p>The main user group for a portal like this is researchers. The prototype is based on input from the research community and experiences from other search portals. The work is carried out in dialog with other research infrastructures.</p>
+                    <p>The main user group for a portal like this is researchers. The prototype is based on input from the research community and experiences from other search portals. The work was carried out in dialog with other research infrastructures.</p>
                 </li>
             </ul>
             <!-- /featured-grid.small-block-grid-1.medium-block-grid-3 -->
