6ca7af7d2f8548372a8b9e48b7a9a2baf3982c03
Olof Olsson <borsna@users.noreply.github.com>
2016-10-18 16:26:48 +0200

    fix alt for SND image

--- resources/views/shared/_in_collaboration.blade.php
+++ resources/views/shared/_in_collaboration.blade.php
@@ -7,7 +7,7 @@
                 <li><a href="http://samfund.dda.dk/dda/default-en.asp" title="Danish Data Archive (DDA)"><img src="{{ asset("/images/content/partners/dda_logo.png") }}" alt="Danish Data Archive (DDA)" /></a></li>
                 <li><a href="http://www.fsd.uta.fi/en/" title="Finnish Social Science Data Archive (FSD)"><img src="{{ asset("/images/content/partners/fsd_logo.png") }}" alt="Finnish Social Science Data Archive (FSD)" /></a></li>
                 <li><a href="http://www.nordforsk.org/en" title="NordForsk"><img src="{{ asset("/images/content/partners/nordforsk_logo.png") }}" alt="NordForsk" /></a></li>
-                <li><a href="http://www.nsd.uib.no/nsd/english/index.html" title="Norwegian Center for Research Data (NSD)"><img src="{{ asset("/images/content/partners/nsd_logo.png") }}" alt="Norwegian Social Science Data Services (NSD)" /></a></li>
+                <li><a href="http://www.nsd.uib.no/nsd/english/index.html" title="Norwegian Center for Research Data (NSD)"><img src="{{ asset("/images/content/partners/nsd_logo.png") }}" alt="Norwegian Center for Research Data (NSD)" /></a></li>
                 <li><a href="http://snd.gu.se/en" title="Swedish National Data Service (SND)"><img src="{{ asset("/images/content/partners/snd_logo.png") }}" alt="Swedish National Data Service (SND)" /></a></li>
             </ul>
             <!-- /small-block-grid-1.medium-block-grid-5 -->
